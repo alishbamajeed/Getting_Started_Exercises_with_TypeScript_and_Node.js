b5915502a7a88cb924c0f37ca1193cb70bf881d6
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 17:02:24 +0500

    Add files via upload
    My 5th Assignments

---
+++ 5) Famous Quote 2/Famous Quote 2.ts.ts
@@ -0,0 +1,5 @@
+let famous_person: string = "Winston Churchill";
+let quote: string = "Success is not final, failure is not fatal: It is the courage to continue that counts.";
+let message: string = `${famous_person} once said, "${quote}"`;
+
+console.log(message);
---
+++ 5) Famous Quote 2/Famous_Quote_2.js.js
@@ -0,0 +1,5 @@
+"use strict";
+let famous_person = "Winston Churchill";
+let quote = "Success is not final, failure is not fatal: It is the courage to continue that counts.";
+let message = `${famous_person} once said, "${quote}"`;
+console.log(message);
