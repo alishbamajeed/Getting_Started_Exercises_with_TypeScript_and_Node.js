fc799b804002defcd6120c5f4587fe42406d9bfd
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 17:12:49 +0500

    Add files via upload
    My 9th Assignments

---
+++ 9) Favorite Number/Favorite_Number.js.js
@@ -0,0 +1,4 @@
+"use strict";
+let favoriteNumber = 2;
+let message = `My favorite number is ${favoriteNumber}.`;
+console.log(message);
---
+++ 9) Favorite Number/Favorite_Number.ts.ts
@@ -0,0 +1,4 @@
+let favoriteNumber: number = 2 ;
+let message: string = `My favorite number is ${favoriteNumber}.`;
+
+console.log(message);
