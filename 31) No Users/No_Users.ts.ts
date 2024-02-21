cc8d295fb40c49bbfcb0319e6822ca30d3f260ba
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 20:06:07 +0500

    Add files via upload
    my 31 assignment

---
+++ 31) No Users/No_Users.ts.ts
@@ -0,0 +1,10 @@
+let users: string[] = []; // empty array of users
+
+// Add an if test to check if the list of users is not empty
+if (users.length > 0) {
+    // Remove all usernames from the array
+    users = [];
+    console.log("All usernames have been removed.");
+} else {
+    console.log("We need to find some users!");
+}
