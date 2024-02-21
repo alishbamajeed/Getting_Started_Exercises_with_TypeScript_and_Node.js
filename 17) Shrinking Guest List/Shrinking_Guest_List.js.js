389b395c8eb253b15dddba690dcce66ad53e3d8d
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 18:10:05 +0500

    Add files via upload
    My 17 Assignment

---
+++ 17) Shrinking Guest List/Shrinking_Guest_List.js.js
@@ -0,0 +1,39 @@
+"use strict";
+// Initial guest list
+let guestList = ["Alishba", "Manahil", "Mariyam", "Nelofar", "Nimmra", "Maheen", "Fatima"];
+// Print initial invitations
+console.log("Initial Invitations to Dinner:");
+guestList.forEach(person => {
+    console.log(`Dear ${person}, you are cordially invited to dinner with me. It would be an honor to have your presence.`);
+});
+console.log("\n");
+// Name of the guest who can't make it
+let cantMakeIt = guestList[1];
+console.log(`Unfortunately, ${cantMakeIt} can't make it to the dinner.\n`);
+// New guests to add
+let newGuests = ["Nelofar", "Alishba", "Manahil"];
+// Replace the guest who can't make it with a new guest
+let indexToRemove = guestList.indexOf(cantMakeIt);
+if (indexToRemove !== -1) {
+    guestList.splice(indexToRemove, 1, ...newGuests);
+}
+// Print updated invitations
+console.log("Updated Invitations to Dinner:");
+guestList.forEach(person => {
+    console.log(`Dear ${person}, you are cordially invited to dinner with me. It would be an honor to have your presence.`);
+});
+console.log("\n");
+// Inform about the limited space
+console.log("Unfortunately, the new dinner table won't arrive in time, so we can only invite two people for dinner.\n");
+// Remove guests until only two names remain in the list
+while (guestList.length > 2) {
+    let removedGuest = guestList.pop();
+    console.log(`Sorry ${removedGuest}, we can't invite you to dinner.`);
+}
+// Print invitation to the remaining two guests
+console.log(`Dear ${guestList[0]} and ${guestList[1]}, you are still invited to dinner with me. It would be an honor to have your presence.\n`);
+// Remove the last two names from the list
+guestList.pop();
+guestList.pop();
+// Print the final empty list
+console.log("Final Guest List:", guestList);
