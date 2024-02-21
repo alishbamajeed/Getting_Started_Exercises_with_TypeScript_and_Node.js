bb17d84187fa19edb60bbdb188b4e70be0ceca03
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 17:29:22 +0500

    Add files via upload
    My 11 Assignments

---
+++ 11) Names/Name.js.js
@@ -0,0 +1,7 @@
+"use strict";
+let names = ["Maria", "Mariyam", "Sakeena", "Areeba"];
+// Printing each person's name by accessing each element in the list
+console.log("Printing names:");
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11) Names/Name.ts.ts
@@ -0,0 +1,7 @@
+let names: string[] = ["Maria", "Mariyam", "Sakeena", "Areeba"];
+
+// Printing each person's name by accessing each element in the list
+console.log("Printing names:");
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
