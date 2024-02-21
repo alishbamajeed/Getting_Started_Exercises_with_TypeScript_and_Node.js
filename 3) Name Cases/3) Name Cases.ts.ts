db66102b96518481fea6ab07652f49f78363bd2d
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 16:56:27 +0500

    Add files via upload
    My 2nd Assignments

---
+++ 3) Name Cases/3) Name Cases.ts.ts
@@ -0,0 +1,14 @@
+// Store the person's name in a variable
+let personName: string = "Ali";
+
+// Print the person's name in lowercase
+console.log("Lowercase:", personName.toLowerCase());
+
+// Print the person's name in uppercase
+console.log("Uppercase:", personName.toUpperCase());
+
+// Print the person's name in titlecase
+console.log("Titlecase:", personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+
+
+
