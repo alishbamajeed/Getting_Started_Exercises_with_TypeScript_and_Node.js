b5951e5f2c97259652d2f2048f3e8138b607921c
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 18:07:00 +0500

    Add files via upload
    My 14 Assignment

---
+++ 14) Guest List/Guest_List.ts.ts
@@ -0,0 +1,7 @@
+let guestList: string[] = ["Maria", "Sakeena", "Mariyam"];
+
+// Printing an invitation message to each person on the guest list
+console.log("Invitations to Dinner:");
+for (let i = 0; i < guestList.length; i++) {
+    console.log(`Dear ${guestList[i]}, you are cordially invited to dinner with me. It would be an honor to have your presence.`);
+}
