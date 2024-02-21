6e376a2f865490cba470e59414723eb115f6476b
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 19:16:02 +0500

    Add files via upload
    My 41 Assignment

---
+++ 41) Magicians/Magicians.js.js
@@ -0,0 +1,8 @@
+"use strict";
+function show_magicians(magicians) {
+    magicians.forEach(magician => console.log(magician));
+}
+// Array of magician's names
+const magicians = ["Harry Houdini", "David Copperfield", "Penn Jillette", "Teller"];
+// Calling the function to show the magicians
+show_magicians(magicians);
---
+++ 41) Magicians/Magicians.ts.ts
@@ -0,0 +1,9 @@
+function show_magicians(magicians: string[]): void {
+    magicians.forEach(magician => console.log(magician));
+}
+
+// Array of magician's names
+const magicians: string[] = ["Harry Houdini", "David Copperfield", "Penn Jillette", "Teller"];
+
+// Calling the function to show the magicians
+show_magicians(magicians);
