919b745b66b843c0a9551c98ad3e0709bccd1cf5
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 18:18:41 +0500

    Add files via upload
    My 20 Assignment

---
+++ 20) River/River.js.js
@@ -0,0 +1,8 @@
+"use strict";
+// Array containing names of famous rivers
+let rivers = ['Jhelum', 'Chenab', 'Ravi', 'Sultej', 'Indus'];
+// Print the list of rivers
+console.log("List of famous rivers:");
+for (let river of rivers) {
+    console.log(river);
+}
---
+++ 20) River/River.ts.ts
@@ -0,0 +1,8 @@
+// Array containing names of famous rivers
+let rivers: string[] = ['Jhelum', 'Chenab', 'Ravi', 'Sultej', 'Indus'];
+
+// Print the list of rivers
+console.log("List of famous rivers:");
+for (let river of rivers) {
+    console.log(river);
+}
