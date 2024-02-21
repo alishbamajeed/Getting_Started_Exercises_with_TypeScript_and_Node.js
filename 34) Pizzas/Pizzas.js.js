24acdde94e7def8c4516fbcdf16df94160e3ab6a
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 19:28:18 +0500

    Add files via upload
    My 34 Assignment

---
+++ 34) Pizzas/Pizzas.js.js
@@ -0,0 +1,6 @@
+"use strict";
+let favoritePizzas = ["Chicken Fajita", "Chicken Superme", "BBQ Chicken"];
+for (let pizza of favoritePizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+console.log("Pizza is one of my favorite foods. I really love pizza!");
---
+++ 34) Pizzas/Pizzas.ts.ts
@@ -0,0 +1,7 @@
+let favoritePizzas: string[] = ["Chicken Fajita", "Chicken Superme", "BBQ Chicken"];
+
+for (let pizza of favoritePizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+
+console.log("Pizza is one of my favorite foods. I really love pizza!");
