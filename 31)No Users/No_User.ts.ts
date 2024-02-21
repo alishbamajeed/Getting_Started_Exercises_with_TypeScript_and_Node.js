b669e1e4548f4f40b6961cdb0d9da9dd46922e8a
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 19:42:25 +0500

    Add files via upload
    My 31 Assignment

---
+++ 31)No Users/No_User.ts.ts
@@ -0,0 +1,13 @@
+let usernames: string[] = [];
+
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+} else {
+    for (let username of usernames) {
+        if (username === "admin") {
+            console.log("Hello admin, would you like to see a status report?");
+        } else {
+            console.log(`Hello ${username}, thank you for logging in again.`);
+        }
+    }
+}
