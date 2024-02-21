4f2b653e06b2b55c9f11e492fbae557078a7c6af
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 19:06:43 +0500

    Add files via upload
    My 29 Assignment

---
+++ 29) Favorite Fruit/Favorite_Fruit.ts.ts
@@ -0,0 +1,25 @@
+let favorite_fruits: string[] = ["banana", "apple", "mango"];
+
+if (favorite_fruits.includes("banana")) {
+    console.log("You really like bananas!");
+}
+
+if (favorite_fruits.includes("apple")) {
+    console.log("You really like apples!");
+}
+
+if (favorite_fruits.includes("mango")) {
+    console.log("You really like mangoes!");
+}
+
+if (favorite_fruits.includes("orange")) {
+    console.log("You really like oranges!");
+} else {
+    console.log("You don't seem to like oranges.");
+}
+
+if (favorite_fruits.includes("kiwi")) {
+    console.log("You really like kiwis!");
+} else {
+    console.log("You don't seem to like kiwis.");
+}
