39b20ee8052c567df75f2da69941b060face95ca
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 19:01:47 +0500

    Add files via upload
    My 26 Assignment

---
+++ 26) Alien Colors #2/Alien_Colors_#2.js.js
@@ -0,0 +1,17 @@
+"use strict";
+//Version running the if block (alien's color is green)://
+let alienColor = "green";
+if (alienColor === "green") {
+    console.log("The player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("The player just earned 10 points.");
+}
+//Version running the else block (alien's color is not green)://
+let AlienColor = "red"; // For example, assuming the alien's color is red
+if (alienColor === "green") {
+    console.log("The player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("The player just earned 10 points.");
+}
---
+++ 26) Alien Colors #2/Alien_Colors_#2.ts.ts
@@ -0,0 +1,19 @@
+//Version running the if block (alien's color is green)://
+
+let alienColor: string = "green";
+
+if (alienColor === "green") {
+    console.log("The player just earned 5 points for shooting the alien.");
+} else {
+    console.log("The player just earned 10 points.");
+}
+//Version running the else block (alien's color is not green)://
+let AlienColor: string = "red"; // For example, assuming the alien's color is red
+
+if (alienColor === "green") {
+    console.log("The player just earned 5 points for shooting the alien.");
+} else {
+    console.log("The player just earned 10 points.");
+}
+
+
