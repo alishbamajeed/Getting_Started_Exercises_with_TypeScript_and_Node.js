17a25e8248ac609f484f5da0f0171cb5fd8c3ba1
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 19:25:30 +0500

    Add files via upload
    My 35 Assignment

---
+++ 35) Animals/Animals.js.js
@@ -0,0 +1,6 @@
+"use strict";
+let animals = ["Parrot", "Canary", "Finch"];
+for (let animal of animals) {
+    console.log(`A ${animal.toLowerCase()} would make a wonderful singing companion.`);
+}
+console.log("These birds have beautiful voices and would enhance any home with their melodies!");
