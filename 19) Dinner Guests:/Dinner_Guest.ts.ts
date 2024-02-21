e4eda60546e8bc74c981faa98f44e277e4025f83
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 18:15:02 +0500

    Add files via upload
    My 19 Assignment

---
+++ 19) Dinner Guests:/Dinner_Guest.ts.ts
@@ -0,0 +1,12 @@
+// Example TypeScript program with a list of dinner guests
+
+let dinnerGuests: string[] = ['Maria', 'Alishba', 'Sakeena', ];
+
+// Print the list of dinner guests
+console.log("List of dinner guests:");
+for (let guest of dinnerGuests) {
+    console.log(guest);
+}
+
+// Print the number of dinner guests
+console.log(`You are inviting ${dinnerGuests.length} people to dinner.`);
