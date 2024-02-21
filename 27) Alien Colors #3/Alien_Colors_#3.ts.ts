b5852a3c23a244822fbcce14b8e281f646644258
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 19:03:20 +0500

    Add files via upload
    My 27 Assignment

---
+++ 27) Alien Colors #3/Alien_Colors_#3.ts.ts
@@ -0,0 +1,27 @@
+let alienColor: string = "green";
+
+if (alienColor === "green") {
+    console.log("The player earned 5 points.");
+} else if (alienColor === "yellow") {
+    console.log("The player earned 10 points.");
+} else if (alienColor === "red") {
+    console.log("The player earned 15 points.");
+}
+alienColor = "yellow";
+
+if (alienColor === "green") {
+    console.log("The player earned 5 points.");
+} else if (alienColor === "yellow") {
+    console.log("The player earned 10 points.");
+} else if (alienColor === "red") {
+    console.log("The player earned 15 points.");
+}
+alienColor = "red";
+
+if (alienColor === "green") {
+    console.log("The player earned 5 points.");
+} else if (alienColor === "yellow") {
+    console.log("The player earned 10 points.");
+} else if (alienColor === "red") {
+    console.log("The player earned 15 points.");
+}
