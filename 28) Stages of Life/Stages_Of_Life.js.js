6250fc574ca9a37ccb258d4fa9e08bb49dea26a0
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 19:05:10 +0500

    Add files via upload
    My 28 Assignment

---
+++ 28) Stages of Life/Stages_Of_Life.js.js
@@ -0,0 +1,22 @@
+"use strict";
+let age = 17;
+switch (true) {
+    case age < 2:
+        console.log("The person is a baby.");
+        break;
+    case age < 4:
+        console.log("The person is a toddler.");
+        break;
+    case age < 13:
+        console.log("The person is a kid.");
+        break;
+    case age < 20:
+        console.log("The person is a teenager.");
+        break;
+    case age < 65:
+        console.log("The person is an adult.");
+        break;
+    default:
+        console.log("The person is an elder.");
+        break;
+}
