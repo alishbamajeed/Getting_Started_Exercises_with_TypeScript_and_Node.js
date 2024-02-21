9ec2ef33d320321ef86c72eddbc05648b5eb91ac
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 19:23:28 +0500

    Add files via upload
    My 36 Assignment

---
+++ 36)T-Shirt/T-Shirt.js.js
@@ -0,0 +1,6 @@
+"use strict";
+function make_shirt(size, message) {
+    console.log(`A ${size} shirt will be printed with the following message: "${message}".`);
+}
+// Calling the function
+make_shirt("Large", "Hello, World!");
---
+++ 36)T-Shirt/T-Shirt.ts.ts
@@ -0,0 +1,6 @@
+function make_shirt(size: string, message: string): void {
+    console.log(`A ${size} shirt will be printed with the following message: "${message}".`);
+}
+
+// Calling the function
+make_shirt("Large", "Hello, World!");
