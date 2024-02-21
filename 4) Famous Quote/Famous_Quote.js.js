e798ac28fb8f0927831b142c3f86c40a025c1bd1
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 16:59:31 +0500

    Add files via upload
    My 4th Assignments

---
+++ 4) Famous Quote/Famous_Quote.js.js
@@ -0,0 +1,4 @@
+"use strict";
+let quote = "The greatest glory in living lies not in never falling, but in rising every time we fall.";
+let author = "Albert Einstein";
+console.log(`${author} once said, "${quote}"`);
---
+++ 4) Famous Quote/Famous_Quote.ts.ts
@@ -0,0 +1,5 @@
+let quote: string = "The greatest glory in living lies not in never falling, but in rising every time we fall.";
+let author: string = "Albert Einstein";
+
+console.log(`${author} once said, "${quote}"`);
+
