e99b78c8e6c51d8ae0b67df2e12c34c22366fcdd
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 18:53:26 +0500

    Add files via upload
    My 23 Assignment

---
+++ 23) Conditional Tests/Conditional_test.ts.ts
@@ -0,0 +1,41 @@
+let car: string = 'subaru';
+
+// Test 1: Is car == 'subaru'? I predict True.
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+
+// Test 2: Is car == 'honda'? I predict False.
+console.log("Is car == 'honda'? I predict False.");
+console.log(car == 'honda');
+
+// Test 3: Is car != 'toyota'? I predict True.
+console.log("Is car != 'toyota'? I predict True.");
+console.log(car != 'toyota');
+
+// Test 4: Is car != 'subaru'? I predict False.
+console.log("Is car != 'subaru'? I predict False.");
+console.log(car != 'subaru');
+
+// Test 5: Is car.length > 0? I predict True.
+console.log("Is car.length > 0? I predict True.");
+console.log(car.length > 0);
+
+// Test 6: Is car.length < 5? I predict False.
+console.log("Is car.length < 5? I predict False.");
+console.log(car.length < 5);
+
+// Test 7: Is car.startsWith('sub')? I predict True.
+console.log("Is car.startsWith('sub')? I predict True.");
+console.log(car.startsWith('sub'));
+
+// Test 8: Is car.includes('aru')? I predict True.
+console.log("Is car.includes('aru')? I predict True.");
+console.log(car.includes('aru'));
+
+// Test 9: Is car.endsWith('ru')? I predict True.
+console.log("Is car.endsWith('ru')? I predict True.");
+console.log(car.endsWith('ru'));
+
+// Test 10: Is car.toUpperCase() == 'SUBARU'? I predict True.
+console.log("Is car.toUpperCase() == 'SUBARU'? I predict True.");
+console.log(car.toUpperCase() == 'SUBARU');
