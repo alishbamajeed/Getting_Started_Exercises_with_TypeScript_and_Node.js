cfdd835907a4de85095418c751d62746151c52fd
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 19:22:11 +0500

    Add files via upload
    My 37 Assignment

---
+++ 37) Large Shirts/Large_Shirts.ts.ts
@@ -0,0 +1,6 @@
+function make_shirt(size: string, message: string): void {
+    console.log(`The shirt size is ${size} and it will be printed with the following message: "${message}".`);
+}
+
+// Calling the function
+make_shirt("Large", "I love TypeScript!");
