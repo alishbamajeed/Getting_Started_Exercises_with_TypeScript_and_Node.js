cbc81de9394b1dc34df744f163a6a67fd68208db
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-21 17:33:20 +0500

    Add files via upload
    My 13 Assignment

---
+++ 13) Your Own Array/Your_Own_Array.ts.ts
@@ -0,0 +1,7 @@
+let Cars: string[] = ["Spots Car", "Coupe", "Convertible"];
+
+// Printing statements about the Car
+console.log("My favorite mode of transportation is a Car.");
+console.log("I would like to own a " + Cars[0] + ".");
+console.log("I dream of driving a " + Cars[1] + ".");
+console.log("I enjoy performing tricks on a " + Cars[2] + ".");
