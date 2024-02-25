43dba6fb2bd990f946673031bb1ea2a86f37eb43
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-02-25 13:29:47 +0500

    Delete personal message directory

--- personal message/2) Personal Message.ts.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”//
-let personName = "Ali";
-console.log(`Hello ${personName}, would you like to learn some Python today?`);
